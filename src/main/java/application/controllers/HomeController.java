b9a988d17a6e2049bdef3a2b9b790421ead0bee5
Nicholas <njriesen@iastate.edu>
2017-01-28 20:28:04 -0600

    Configure basic user authentication

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package application;
+package application.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +9,13 @@ public class HomeController {
 	@RequestMapping("/")
 	public String showHomePage(){
 		
+		return "home";
+	};
+
+
+	@RequestMapping("/userHome")
+	public String showUserHome(){
+
 		return "userHome";
 	};
 
